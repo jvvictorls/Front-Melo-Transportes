28d18d8704f017be8e84e136872168478f072cc0
João Victor <jvvictorls@hotmail.com>
2024-09-09 10:44:08 -0300

    Fix: Layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,45 +3,56 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="flex border shadow-sm justify-evenly items-center">
-        <div className=''>
-          <Link
-            to="/dashboard"
-            className='flex justify-center items-center'
+    <header className='w-full flex items-center justify-center shadow-md'>
+        <div className="w-3/4 flex  justify-evenly items-center">
+          <div className='ml-4'>
+            <Link
+              to="/dashboard"
+              className='flex justify-center items-center'
+            >
+              <img
+                src="/src/public/recropped-melo.png"
+                alt="melo_transportes"
+                className="h-6 mb-2"
+              />
+            </Link>
+          </div>
+          
+                <nav className='flex-1 flex items-center justify-center  justify-evenly items-center '>
+          <NavLink
+          to='/'
+          className='mb-2'
           >
-            <img
-              src="/src/public/recropped-melo.png"
-              alt="melo_transportes"
-              className="h-6 mb-2"
-            />
-          </Link>
+            Home
+          </NavLink>
+          <NavLink
+          to='/request'
+          className='mb-2'
+          >
+            Solicitar Rota
+          </NavLink>
+          <NavLink
+          to='routes'
+          className='mb-2'
+          >
+            Rotas
+          </NavLink>
+                </nav>
+          
+                <div 
+                className='w-1/6 flex items-center justify-between'>
+          <button 
+          className=''
+          >
+            Entrar
+          </button>
+          <button
+          className=''
+          >
+            Registre-se
+          </button>
+                </div>
         </div>
-
-      <nav className='w-1/3 flex items-center justify-center  justify-evenly items-center '>
-        <NavLink 
-        to='/'
-        className='mb-2'
-        >
-          Home
-        </NavLink>
-        <NavLink
-        to='/request'
-        className='mb-2'
-        >
-          Solicitar Rota
-        </NavLink>
-        <NavLink
-        to='routes'
-        className='mb-2'
-        >
-          Rotas
-        </NavLink>  
-      </nav>
-
-      <div 
-      className='h-full w-1/12 flex items-center justify-center'>
-        <button className=''>Login</button>
-      </div>
     </header>
   );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 function Layout() {
   return (
-    <div> 
-      < Header />
-      <Outlet />
+    <div className='flex flex-col h-screen'> 
+      <Header />
+      <div className='flex-grow'>
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
