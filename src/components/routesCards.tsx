a958d979a0b29390acc8fd0633dcf507c55f15cd
João Victor <jvvictorls@hotmail.com>
2024-11-21 11:15:22 -0300

    Fix: Added navigation to routeDetails

--- src/components/routesCards.tsx
+++ src/components/routesCards.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+
 type RoutesCardsProps = {
   routes: never[]
 };
-export default function routesCards({ routes }: RoutesCardsProps) {
+export default function RoutesCards({ routes }: RoutesCardsProps) {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 xl:grid xl:grid-cols-3 2xl:grid 2xl:grid-cols-3">
       {routes.map((route: any) => (
@@ -10,7 +13,7 @@ export default function routesCards({ routes }: RoutesCardsProps) {
           className="w-full flex justify-center h-full"
         >
           <div
-            className="p-4 border border-sm shadow-sm xs:w-11/12 md:w-11/12 lg:w-11/12"
+            className="flex flex-col space-evenly p-4 border border-sm shadow-sm w-full"
           >
             <h1
               className="text-center border-b-2"
@@ -37,6 +40,7 @@ export default function routesCards({ routes }: RoutesCardsProps) {
             >
               <button
                 className="bg-blue-500 w-2/3 md:w-2/3 text-center text-white hover:bg-blue-300 rounded-lg p-2 m-2"
+                onClick={ () => navigate(`/routes/${route.id}`) }
               >
                 Ver detalhes
               </button>
