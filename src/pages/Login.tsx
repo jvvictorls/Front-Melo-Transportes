a5422b3bf1e4389b01ac2cb3f38391c84eed418c
João Victor <jvvictorls@hotmail.com>
2025-01-14 14:53:55 -0300

    Feat: Context for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Navigate, Link } from 'react-router-dom';
 import { requestLogin } from '../services/request';
+import AuthContext from '../context/AuthContext';
 
 function Login() {
+  const { auth, setAuth } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLogged, setIsLogged] = useState(false);
@@ -16,9 +18,11 @@ function Login() {
       await requestLogin('/users/login', { email, password });
       setIsLogged(true);
       setLoginError(false);
+      setAuth(true);
     } catch (error) {
       setIsLogged(false);
       setLoginError(true);
+      setAuth(false);
     }
   };
 
